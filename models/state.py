6ce9fa85181a01d9b4e1e4b6281beec52b736765
VectorGits <davidoluremi0@gmail.com>
2024-04-17 23:53:58 +0100

    Add SQLAlchemy support to State class in models/state.py

--- models/state.py
+++ models/state.py
@@ -1,8 +1,24 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
+from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
+from models.city import City
+from models import storage
+from os import getenv
 
 
 class State(BaseModel):
     """ State class """
-    name = ""
+    name = Column(String(128), nullable=False)
+    if getenv("HBNB_TYPE_STORAGE") == "db":
+        cities = relationship("City", backref="state", cascade="all, delete")
+    else:
+        @property
+        def cities(self):
+            """ Getter attribute in case of file storage """
+            city_list = []
+            for city in storage.all(City).values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
