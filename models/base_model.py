751b46560e5319c67bd86d3d18b3f999a4e4fd35
VectorGits <davidoluremi0@gmail.com>
2024-04-17 23:58:28 +0100

    Refactor file to fit w/ pycodestyle guidelines

--- models/base_model.py
+++ models/base_model.py
@@ -42,7 +42,7 @@ class BaseModel:
         if '_sa_instance_state' in dictionary:
             del dictionary['_sa_instance_state']
         return dictionary
-    
+
     def delete(self):
         """Delete the current instance from the storage"""
         from models import storage
